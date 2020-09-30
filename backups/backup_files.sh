4f0237a6f7fc1716604876235e76696b3cd909f4
Skunnyk <skunnyk@alteroot.org>
2020-09-30 11:42:13 +0200

    Improve a bit backups scripts

--- backups/backup_files.sh
+++ backups/backup_files.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_PATH=$(dirname $0)
+SCRIPT_PATH=$(dirname "$0")
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/root/bin
-YESTERDAY=$(date -d "1 day ago" +%Y%m%d)
+YESTERDAY="$(date -d "1 day ago" +%Y%m%d)"
 HISTORY=3
-OLDBACKUP=$(date -d "$HISTORY day ago" +%Y%m%d) 
+OLDBACKUP="$(date -d "$HISTORY day ago" +%Y%m%d)"
 
 # Source the config file with all path / usernames
-if [ -f $SCRIPT_PATH/backup.conf ]; then
-        source $SCRIPT_PATH/backup.conf
+if [ -f "${SCRIPT_PATH}/backup.conf" ]; then
+        source "${SCRIPT_PATH}/backup.conf"
 else 
 	echo "Please create a backup.conf from backup.conf-dist with correct variables"
 fi
 
 # On vire les backups d'il y a $HISTORY jours
-if [ -d ${LOCAL_BACKUP_DIR}/web/$OLDBACKUP ]; then
-        rm -rf ${LOCAL_BACKUP_DIR}/web/$OLDBACKUP
+if [ -d "${LOCAL_BACKUP_DIR}/web/${OLDBACKUP}" ]; then
+        rm -rf "${LOCAL_BACKUP_DIR}/web/${OLDBACKUP}"
 fi
 
 # On copie avec hardlink le backup de la veille
-if [ ! -d ${LOCAL_BACKUP_DIR}/web/$YESTERDAY ]; then
-        cp -al ${LOCAL_BACKUP_DIR}/web/today/ ${LOCAL_BACKUP_DIR}/web/$YESTERDAY
+if [ ! -d "${LOCAL_BACKUP_DIR}/web/${YESTERDAY}" ]; then
+        cp -al "${LOCAL_BACKUP_DIR}/web/today/" "${LOCAL_BACKUP_DIR}/web/${YESTERDAY}"
 fi
 
 # et on rsync :)
-rsync -aHq --delete ${LOCAL_SOURCE_DIR} ${LOCAL_BACKUP_DIR}/web/today/
-rsync -aHq --delete ${LOCAL_BACKUP_DIR}/web/ ${SSH_REMOTE}:${SSH_DESTDIR}/web/
+rsync -aHq --delete --exclude="${RSYNC_EXCLUDE}" "${LOCAL_SOURCE_DIR}" "${LOCAL_BACKUP_DIR}/web/today/"
+rsync -aHq --delete "${LOCAL_BACKUP_DIR}/web/" "${SSH_REMOTE}:${SSH_DESTDIR}/web/"
 
-tar czf ${LOCAL_BACKUP_DIR}/etc/whole_etc.$YESTERDAY.tar.gz /etc/ &> /dev/null
-rsync -aHq --delete ${LOCAL_BACKUP_DIR}/etc/ ${SSH_REMOTE}:${SSH_DESTDIR}/etc
+tar czf "${LOCAL_BACKUP_DIR}/etc/whole_etc.$YESTERDAY.tar.gz" /etc/ &> /dev/null
+rsync -aHq --delete "${LOCAL_BACKUP_DIR}/etc/" "${SSH_REMOTE}:${SSH_DESTDIR}/etc"
 
--- backups/backup_sql.sh
+++ backups/backup_sql.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_PATH=$(dirname $0)
+SCRIPT_PATH=$(dirname "$0")
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 # Source the config file with all path / usernames
-if [ -f $SCRIPT_PATH/backup.conf ]; then
-        source $SCRIPT_PATH/backup.conf
+if [ -f "${SCRIPT_PATH}/backup.conf" ]; then
+  source "${SCRIPT_PATH}/backup.conf"
 else 
 	echo "Please create a backup.conf from backup.conf-dist with correct variables"
 fi
 # MySQL
-LOCAL_BACKUP_DIR="${LOCAL_DIR}/$(date +%Y%m%d)"
+OLDBACKUP="$(date -d "${HISTORY} day ago" +%Y%m%d)"
+LOCAL_BACKUP_DIR="${LOCAL_BACKUP_DIR}/mysql/$(date +%Y%m%d)"
 
-mkdir -p $FOLDER
-for database in `mysql -e "show databases" --skip-column-names`; do 
-        mysqldump --opt ${database} > ${LOCAL_DEST_DIR}/${database}.sql; 
-        bzip2 ${LOCAL_BACKUP_DIR}/${database}.sql
+mkdir -p "${LOCAL_BACKUP_DIR}"
+for database in $(mysql -e "show databases" --skip-column-names); do
+  mysqldump --single-transaction --opt "${database}" > "${LOCAL_BACKUP_DIR}/${database}.sql";
+  bzip2 "${LOCAL_BACKUP_DIR}/${database}.sql"
 done
-cp /etc/mysql/my.cnf ${LOCAL_BACKUP_DIR}/
-rsync -aHq --delete-after ${LOCAL_BACKUP_DIR} ${SSH_REMOTE}:${SSH_DESTDIR}/mysql
+cp /etc/mysql/my.cnf "${LOCAL_BACKUP_DIR}/"
+rsync -aHq --delete-after "${LOCAL_BACKUP_DIR}" "${SSH_REMOTE}:${SSH_DESTDIR}/mysql"
+
+# Delete old backups
+if [ -d "${LOCAL_BACKUP_DIR}/mysql/$OLDBACKUP" ]; then
+  rm -rf "${LOCAL_BACKUP_DIR}/mysql/$OLDBACKUP"
+fi
